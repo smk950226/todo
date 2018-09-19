fa2842f6bb4172aa8d7416c23b8c7990bdffc8d1
SEUNGMIN KIM <smk950226@naver.com>
2018-09-20 02:42:46 +0900

    디자인4 버튼 추가

--- ToDo.js
+++ ToDo.js
@@ -9,13 +9,37 @@ export default class ToDo extends Component{
         isCompleted: false
     }
     render(){
-        const {isCompleted} = this.state;
+        const {isCompleted, isEditing} = this.state;
         return (
         <View style={styles.container}>
-        <TouchableOpacity onPress={this._toggleComplete}>
-            <View style={[styles.circle, isCompleted ? styles.completedCircle : styles.uncompletedCircle]} />
-        </TouchableOpacity>
-            <Text style={styles.text}>Hello</Text>
+            <View style={styles.column}>
+                <TouchableOpacity onPress={this._toggleComplete}>
+                    <View style={[styles.circle, isCompleted ? styles.completedCircle : styles.uncompletedCircle]} />
+                </TouchableOpacity>
+                <Text style={[styles.text, isCompleted ? styles.completedText : styles.uncompletedText]}>Hello</Text>
+            </View>
+                {isEditing ? (
+                    <View style={styles.actions}>
+                        <TouchableOpacity onPressOut={this._finishEditing}>
+                            <View style={styles.actionContainer}>
+                                <Text style={styles.actionText}>V</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                ) : (
+                    <View style={styles.actions}>
+                        <TouchableOpacity onPressOut={this._startEditing}>
+                            <View style={styles.actionContainer}>
+                                <Text style={styles.actionText}>Edit</Text>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity>
+                            <View style={styles.actionContainer}>
+                                <Text style={styles.actionText}>Del</Text>
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                )}
         </View>
         )
     };
@@ -24,6 +48,16 @@ export default class ToDo extends Component{
             return({
                 isCompleted: !prevState.isCompleted
             })
+        })
+    };
+    _startEditing = () => {
+        this.setState({
+            isEditing: true
+        })
+    };
+    _finishEditing = () => {
+        this.setState({
+            isEditing: false
         })
     }
 }
@@ -34,7 +68,8 @@ const styles = StyleSheet.create({
         borderBottomColor: '#bbb',
         borderBottomWidth: StyleSheet.hairlineWidth,
         flexDirection: 'row',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'space-between'
     },
     text: {
         fontWeight: '600',
@@ -54,5 +89,25 @@ const styles = StyleSheet.create({
     },
     uncompletedCircle: {
         borderColor: '#f23657'
+    },
+    completedText: {
+        color: '#bbb',
+        textDecorationLine: 'line-through'
+    },
+    uncompletedText: {
+        color: '#353535'
+    },
+    column: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: width / 2,
+        justifyContent: 'space-between',
+    },
+    actions: {
+        flexDirection: 'row'
+    },
+    actionContainer: {
+        marginVertical: 10,
+        marginHorizontal: 10
     }
 })
